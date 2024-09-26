04ee084e238fad243dc19983581b9a06832949ff
drudante <drudante@gmail.com>
2024-09-25 21:00:37 -0700

    Updated App.js with a few small changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
+
 import NavBar from "./components/NavBar";
 
 function App() {
