7feb67d73010ab27bb0ffedc3eaa050ed39a085f
drudante <drudante@gmail.com>
2024-12-11 06:27:03 -0800

    Styled event card to resemble Airbnb event cards

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,41 +1,115 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography, CardActionArea, Button, CardActions } from "@mui/material";
+import { Card, CardActionArea, Button, CardActions, Box, Typography, CardMedia } from "@mui/material";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
 
 function EventCard({ event, createEventMode, onAddProvider }) {
-  const formattedStartDate = format(new Date(event.startDate), "MMMM d, yyyy");
-  const formattedEndDate = format(new Date(event.endDate), "MMMM d, yyyy");
+  const formattedStartDate = format(new Date(event.startDate), "MMM d");
+  const formattedEndDate = format(new Date(event.endDate), "MMM d");
 
   const handleAddClick = () => {
     onAddProvider(event);
   };
 
+  const formattedPrice = new Intl.NumberFormat("en-US").format(Math.round(event.price));
+
   return (
-    <Card>
-      <CardActionArea component={createEventMode ? "div" : Link} to={createEventMode ? undefined : `/events/${event.id}`} sx={{ cursor: createEventMode ? "default" : "pointer" }}>
-        <CardMedia component="img" height="140" image={event.image} alt={event.title} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+    <Card
+      sx={{
+        borderRadius: 4,
+        boxShadow: "none",
+        position: "relative",
+        overflow: "visible",
+      }}
+    >
+      <CardActionArea
+        component={createEventMode ? "div" : Link}
+        to={createEventMode ? undefined : `/events/${event.id}`}
+        sx={{
+          cursor: createEventMode ? "default" : "pointer",
+          display: "block",
+          position: "relative",
+        }}
+      >
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            height: 0,
+            paddingTop: "100%",
+            borderRadius: "16px",
+            overflow: "hidden",
+          }}
+        >
+          <CardMedia
+            image={event.image}
+            component="div"
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: "16px",
+            }}
+          />
+        </Box>
+
+        <Box
+          sx={{
+            position: "relative",
+            zIndex: 1,
+            mt: 1,
+            ml: 1.5,
+            mr: 1.5,
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              mb: "2px",
+              lineHeight: 1.2,
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
             {event.title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              mb: "2px",
+              lineHeight: 1.2,
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
             {event.description}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Location: {event.location.city}, {event.location.state}
+
+          <Typography
+            variant="body2"
+            sx={{
+              lineHeight: 1.2,
+              textDecoration: "underline",
+              fontWeight: "bold",
+            }}
+          >
+            ${formattedPrice} before taxes
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Dates: {formattedStartDate} - {formattedEndDate}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Price: ${event.price.toFixed(2)}
-          </Typography>
-        </CardContent>
+        </Box>
       </CardActionArea>
+
       {createEventMode && (
-        <CardActions>
-          <Button variant="contained" color="success" fullWidth onClick={handleAddClick}>
+        <CardActions sx={{ mt: 1 }}>
+          <Button variant="contained" color="success" fullWidth onClick={handleAddClick} sx={{ borderRadius: "0 0 16px 16px" }}>
             Add
           </Button>
         </CardActions>
